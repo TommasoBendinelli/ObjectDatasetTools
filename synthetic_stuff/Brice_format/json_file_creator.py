80aff98de888edae707d4d42456d35f538f2579c
TommasoBendinelli <tommaso.bendinelli@gmail.com>
2020-04-06 15:04:03 +0200

    Syn

---
+++ synthetic_stuff/Brice_format/json_file_creator.py
@@ -0,0 +1,30 @@
+INFO = {
+    "description": "Workpiece Dataset",
+    "url": "https://github.com/TommasoBendinelli",
+    "version": "0.1.0",
+    "year": 2018,
+    "contributor": "Brice, Tommaso",
+    "date_created": datetime.datetime.utcnow().isoformat(' ')
+}
+
+LICENSES = [
+    {
+        "id": 1,
+        "name": "Attribution-NonCommercial-ShareAlike License",
+        "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/"
+    }
+]
+
+CATEGORIES = [
+    {
+        'id': 1,
+        'name': 'siemens',
+        'supercategory': 'shape',
+    },
+    {
+        'id': 2,
+        'name': 'phoenix',
+        'supercategory': 'shape',
+    },
+]
+
